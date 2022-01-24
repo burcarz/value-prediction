aaa9f1ac1a1870e960959bc69ddaca99b368a3f5
burcar <zacharyburcar@gmail.com>
2022-01-24 02:57:17 -0500

    circles shooting:

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const Game = require('./utils/game');
 
 const loadMainWindow = () => {
     const mainWindow = new BrowserWindow({
-        width : 1100,
-        height: 700,
+        width : 1920,
+        height: 1080,
         webPreferences: {
             nodeIntegration: true
         }
     });
 
-    mainWindow.loadFile(path.join(__dirname, "index.html"));
+    mainWindow.loadFile(path.join(__dirname, "canvas.html"));
 }
 
 app.on("ready", loadMainWindow);
